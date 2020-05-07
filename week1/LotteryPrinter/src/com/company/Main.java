73c5c00aba58a4964053bd7c3c75bfe9daa56461
hchenfeng <hchenfeng@users.noreply.github.com>
2020-05-07 13:39:16 -0400

    Add random int selector

--- week1/LotteryPrinter/src/com/company/Main.java
+++ week1/LotteryPrinter/src/com/company/Main.java
@@ -1,8 +1,20 @@
+/**
+ * ******************
+ * Assignment 1, CIS 500
+ * Chenfeng Hao
+ * ******************/
+
 package com.company;
 
-public class Main {
+import java.security.SecureRandom;
+import java.util.Arrays;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        SecureRandom secureRandom = new SecureRandom();
+        // Pick 6 random numbers between 1 and 49 (inclusive)
+        int[] number = secureRandom.ints(1, 50).distinct().limit(6).toArray();
+        System.out.println("Play this combination--it'll make you rich!");
+        System.out.println(Arrays.toString(number));
     }
 }
