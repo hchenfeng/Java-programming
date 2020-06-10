46c580add4d2e57b1a5022cf69c3dc7eb593c075
hchenfeng <hchenfeng@users.noreply.github.com>
2020-06-10 09:59:08 -0400

    Initialize project for lab5

---
+++ lab5/src/com/company/ScoreList.java
@@ -0,0 +1,60 @@
+package com.company;
+
+/**
+ * A ScoreList holds a sequence of integer values. It provides
+ * several of the methods for manipulating a list of scores.
+ */
+public class ScoreList {
+
+    private final int MaxLength = 50;
+    private int[] list;        // a list of scores
+    private int size;        // the number of scores in list
+
+    /**
+     * Allocates an array of length 50 for the list.
+     * Initializes size to zero to indicate an empty list.
+     */
+    public ScoreList() {
+
+    }
+
+    /**
+     * Returns the actual number of scores in array list.
+     *
+     * @return the actual size of the list
+     */
+    public int getSize() {
+
+    }
+
+    /**
+     * Reads scores into the array and keeps track of the number
+     * Stops reading when the array is full or no more input.
+     */
+    public void loadList() {
+
+
+    }
+
+    /**
+     * Searches the list for a score and returns its index
+     * if found in the list or -1 otherwise.
+     *
+     * @param num the target of the search
+     * @return an array index or -1
+     */
+    public int findScore(int num) {
+
+
+    }
+
+    /**
+     * Converts a list of scores to a String.
+     *
+     * @return a String representation of the list
+     */
+    public String toString() {
+
+
+    }
+}
---
+++ lab5/src/com/company/ScoreListDemo.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class ScoreListDemo {
+    public static void main(String[] args) {
+        ScoreList list = new ScoreList();
+        // load the list with input values
+        // report the number of scores in the list
+        // prompt the user for a score, search the list
+        // for the input score, and report the result
+        // display all the scores in the list
+    }
+}
